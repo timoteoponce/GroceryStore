5a790c2ddf75c39a8cb75cc5ec453f6fbabdd07a
Timoteo Ponce <ponce.timoteo@gmail.com>
2021-08-27 11:18:54 -0700

    Added simple model Customer and simple unit test for it

---
+++ GroceryStore.Tests/Model/CustomerTest.cs
@@ -0,0 +1,16 @@
+using GroceryStore.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GroceryStore.Tests
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void TestValidAgeAssignment()
+        {
+            var c = new Customer { Id = 1, FirstName = "Kevin", LastName = "Arnold", Age = 16 };
+            Assert.AreEqual(16, c.Age);
+        }
+    }
+}
